3582dc0a78a2d3cfffdd1552c4720b60a3845339
Byron Hall <byronhall@MacBook-Pro.local>
2020-05-22 21:28:11 -0500

    Template Syntax

---
+++ src/components/TemplateSyntax.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+      <!-- One time interpolations -->
+      <div v-once>msg: {{msg}}</div>
+      <button @click="msg+='a'">Try to change msg</button>
+      <!-- Raw HTML -->
+      <p>Using mustache: {{rawHtml}}</p>
+      <p>Using v-html directive: <span v-html="rawHtml"></span></p>
+      <!-- Attributes -->
+      <div :id="dynamicId">
+          This div has an attribute named `id`
+      </div>
+      <button :disabled="isButtonDisabled">Button</button>
+      <!-- Directives -->
+      <p>Now you <span v-if="seen">see</span> me</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "TemplateSyntax",
+    data: function(){
+        return {
+            msg: "Hello World!",
+            rawHtml:`
+                <span style="color: red">
+                    This should be red.
+                </span>
+            `,
+            dynamicId: Math.floor(Math.random() * 10),
+            isButtonDisabled: true,
+            seen: false,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,14 +2,16 @@ import Vue from 'vue'
 // import App from './App.vue'
 
 // import Introduction from './components/Introduction'
-import Instance from './components/Instance'
-import obj2 from './assets/Data.js'
+// import Instance from './components/Instance'
+// import obj2 from './assets/Data.js'
+
+import TemplateSyntax from './components/TemplateSyntax'
 
 Vue.config.productionTip = false
 
-const vm = new Vue({
-  render: h => h(Instance),
+new Vue({
+  render: h => h(TemplateSyntax),
 }).$mount('#app');
 
-console.log(Instance.data() === obj2);
-console.log(vm.$el === document.getElementById("#InstanceVM"));
+// console.log(Instance.data() === obj2);
+// console.log(vm.$el === document.getElementById("#InstanceVM"));
